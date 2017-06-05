50e57c244438b10b432d51a01cf43f6bbe7f2656
Alex Haak <alex_haak@hotmail.co.uk>
2017-06-05 14:54:43 +0100

    Added firefly visualisation and render improvements

--- workers/theia/src/index.js
+++ workers/theia/src/index.js
@@ -5,11 +5,17 @@ let visualise = require('./generated/sandbox/Visualise.js').Visualise;
 let entityacl = require('./generated/improbable/EntityAcl.js').EntityAcl;
 
 let links = require('./generated/sandbox/Links.js').Links;
+let firefly = require('./generated/sandbox/Firefly.js').Firefly;
 
+// specify what you want to observe
 let listenItems = [
     {
         key: 'links',
         component: links
+    },
+    {
+        key: 'firefly',
+        component: firefly
     }
 ];
 
@@ -40,6 +46,11 @@ let canvasOffset = {
     y: 500
 }
 
+let SCALE_REFRESH = 1000;
+let MAX_FPS = 10;
+let renderTimeout = null;
+let scaleRefreshTimeout = null;
+
 const locator = sdk.Locator.create(sdk.DefaultConfiguration.LOCAL_DEVELOPMENT_LOCATOR_URL, locatorParameters);
 locator.getDeploymentList((err, deploymentList) => {
   locator.connect("sandbox", connectionParameters, (err, queueStatus) => {
@@ -67,57 +78,83 @@ locator.getDeploymentList((err, deploymentList) => {
             x: op.data.position.x,
             y: -op.data.position.z,
         }
+
+        setScale();
       });
 
       listenItems.forEach((listenItem) => {
         console.log("======> registering position callback" + listenItem.component.COMPONENT.getComponentId());
 
-        dispatcher.onAddComponent(listenItem.component.COMPONENT, op => {
-            window.entities[op.entityId] = window.entities[op.entityId] || { id: op.entityId };
-            window.entities[op.entityId][listenItem.key] = op.data;
-        });
+        dispatcher.onAddComponent(listenItem.component.COMPONENT, addComponentState.bind(null, listenItem.key));
+        dispatcher.onComponentUpdate(listenItem.component.COMPONENT, updateComponentState.bind(null, listenItem.key));
       });
 
       connection.attachDispatcher(dispatcher);
-
-      setTimeout(() => {
-        setScale();
-        render();
-      }, 5000);
     });
 });
 
+addComponentState = (componentKey, op) => {
+    window.entities[op.entityId] = window.entities[op.entityId] || { id: op.entityId };
+    window.entities[op.entityId][componentKey] = op.data;
+
+    render();
+}
+
+updateComponentState = (componentKey, op) => {
+    window.entities[op.entityId] = window.entities[op.entityId] || { id: op.entityId };
+    
+    window.entities[op.entityId][componentKey].illuminated = op.update.illuminated.value;
+
+    render();
+}
+
 render = () => {
-    renderBackground();
-    renderAllEntities(window.entities);
+    // console.log("Requesting render");
+    if (!renderTimeout) {
+        // console.log("Setting render callback");
+        renderTimeout = setTimeout(() => {
+            renderTimeout = null;
+            // console.log("Actually rerendering");
+            renderBackground();
+            renderAllEntities(window.entities);
+        }, 1000/MAX_FPS)
+    } else {
+        // console.log("Render not needed");
+    }
 };
 
 renderAllEntities = (entities) => {
     for (let id in entities) {
-        renderSingleEntity(entities[id])
+        if (entities[id]) {
+            renderSingleEntity(entities[id])
+        }
     }
 }
 
 renderSingleEntity = (entity) => {
     let position = entity.position;
 
-    ctx.globalAlpha = 1;
-    ctx.fillStyle = "#FFF";
-    ctx.font = 'lighter 10px sans-serif';
-    ctx.fillText(entity.id, getWorldX(position.x) + 5, getWorldY(position.y));
+    if (position) {
+        // ctx.globalAlpha = 1;
+        // ctx.fillStyle = "#FFF";
+        // ctx.font = 'lighter 10px sans-serif';
+        // ctx.fillText(entity.id, getWorldX(position.x) + 5, getWorldY(position.y));
 
-    ctx.strokeStyle = "#FFF";
-    ctx.globalAlpha = 0.5;
+        ctx.globalAlpha = 0.5;
 
-    ctx.beginPath();
-    ctx.arc(getWorldX(position.x), getWorldY(position.y), 3, 0, 2 * Math.PI);
-    ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(getWorldX(position.x), getWorldY(position.y), 8, 0, 2 * Math.PI);
+        ctx.stroke();
 
-    console.log(entity.id)
+        if (entity.firefly && entity.firefly.illuminated) {
+            ctx.fillStyle = "#EDD300";
+            ctx.fill();
+        }
 
-    entity.links.ids.filter(id => id).forEach(targetId => 
-        drawEntityReference(entity, entities[targetId])
-    );
+        entity.links && entity.links.ids.filter(id => id).forEach(targetId => 
+            drawEntityReference(entity, entities[targetId])
+        );
+    }
 }
 
 getWorldX = x => (x * canvasScale.x) + (canvasOffset.x * canvasScale.x);
@@ -125,11 +162,14 @@ getWorldX = x => (x * canvasScale.x) + (canvasOffset.x * canvasScale.x);
 getWorldY = y => (y * canvasScale.y) + (canvasOffset.y * canvasScale.y);
 
 drawEntityReference = (entity1, entity2) => {
-    console.log("2) drawing to: " + entity2.id)
-    ctx.beginPath();
-    ctx.moveTo(getWorldX(entity1.position.x), getWorldY(entity1.position.y));
-    ctx.lineTo(getWorldX(entity2.position.x), getWorldY(entity2.position.y));
-    ctx.stroke();
+    if (entity1 && entity1.position && entity2 && entity2.position) {
+        ctx.strokeStyle = "#FFF";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(getWorldX(entity1.position.x), getWorldY(entity1.position.y));
+        ctx.lineTo(getWorldX(entity2.position.x), getWorldY(entity2.position.y));
+        ctx.stroke();
+    }
 }
 
 initialisePage = () => {
@@ -146,40 +186,46 @@ initialisePage = () => {
 };
 
 setScale = () => {
-    let min = {
-        x: 99999,
-        y: 99999,
+    if (!scaleRefreshTimeout) {
+        scaleRefreshTimeout = null;
+        console.log("Resetting scale");
+        scaleRefreshTimeout = setTimeout(() => {
+            let min = {
+                x: 99999,
+                y: 99999,
+            }
+
+            let max = {
+                x: -99999,
+                y: -99999,
+            }
+
+            for (let id in window.entities) {
+                let position = window.entities[id].position;
+
+                if (position) {
+                    min.x = (position.x < min.x) ? position.x : min.x;
+                    min.y = (position.y < min.y) ? position.y : min.y;
+                    max.x = (position.x > max.x) ? position.x : max.x;
+                    max.y = (position.y > max.y) ? position.y : max.y;    
+                }
+            }
+
+            const bezel = 0; //px
+
+            canvasOffset.x = -min.x + bezel;
+            canvasOffset.y = -min.y + bezel;
+
+            canvasScale.x = (ctx.canvas.width - (bezel*2)) / (max.x - min.x);
+            canvasScale.y = (ctx.canvas.height - (bezel*2)) / (max.y - min.y);
+
+            if (FIX_PERSPECTIVE) {
+                let minScaleFactor = Math.min(canvasScale.x, canvasScale.y);    
+                canvasScale.x = minScaleFactor;
+                canvasScale.y = minScaleFactor;
+            }
+        }, SCALE_REFRESH);
     }
-
-    let max = {
-        x: -99999,
-        y: -99999,
-    }
-
-    for (let id in window.entities) {
-        let position = window.entities[id].position;
-        min.x = (position.x < min.x) ? position.x : min.x;
-        min.y = (position.y < min.y) ? position.y : min.y;
-        max.x = (position.x > max.x) ? position.x : max.x;
-        max.y = (position.y > max.y) ? position.y : max.y; 
-    }
-
-    const bezel = 0; //px
-
-    canvasOffset.x = -min.x + bezel;
-    canvasOffset.y = -min.y + bezel;
-
-    canvasScale.x = (ctx.canvas.width - (bezel*2)) / (max.x - min.x);
-    canvasScale.y = (ctx.canvas.height - (bezel*2)) / (max.y - min.y);
-
-    if (FIX_PERSPECTIVE) {
-        let minScaleFactor = Math.min(canvasScale.x, canvasScale.y);    
-        canvasScale.x = minScaleFactor;
-        canvasScale.y = minScaleFactor;
-    }
-
-    console.log(canvasScale.x, canvasScale.y)
-    console.log(canvasOffset.x, canvasOffset.y)
 }
 
 renderBackground = () => {
