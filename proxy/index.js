883c9e98fd6312a6803fb377842075d29e57e483
Alex Haak <alex_haak@hotmail.co.uk>
2017-06-07 15:41:52 +0100

    Cleanup and remote kill

--- proxy/index.js
+++ proxy/index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var child = require('child_process');
 
-let spatialProcess = null;
+let spatial = null;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
@@ -12,34 +12,46 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    socket.on('request launch', () => {
-        if (startSpatial(socket)) {
-            socket.emit('console dump', "Initiating spatial launch...");
-        } else {
-            socket.emit('console dump', "[ERROR] Spatial already running!");
-        }
-    })
+    socket.on('request launch', () => requestLaunchSpatial(socket));
 
-
+    socket.on('request kill', () => killSpatial(socket));
 });
 
-function startSpatial(socket) {
-    if (!spatialProcess) {
-        spatial = child.spawn('bash', ['startspatial'], []);
+function requestLaunchSpatial(socket) {
+    socket.emit('console dump', "Initiating spatial launch...");
 
-        spatial.stdout.on('data', data => {
-            console.log(data.toString());
-            socket.emit('console dump', data.toString());
-        });
-
-        spatial.stderr.on('data', err => {
-            socket.emit('console dump', err.toString());
-        })
-
-        return true;
+    if (!spatial) {
+        launchSpatial(socket);
+    } else {
+        socket.emit('console dump', "[ERROR] Spatial already running!");
     }
+}
 
-    return false;
+function launchSpatial(socket) {
+    spatial = child.spawn('bash', ['startspatial'], []);
+
+    spatial.stdout.on('data', data => {
+        console.log(data.toString());
+        socket.emit('console dump', data.toString());
+    });
+
+    spatial.stderr.on('data', err => {
+        socket.emit('console dump', err.toString());
+    })
+
+    spatial.on('close', (code, signal) => {
+        socket.emit('console dump', "Spatial died/killed (rip)!");
+        console.log(`child process terminated due to receipt of signal ${signal}`);
+    });
+}
+
+function killSpatial(socket) {
+    if (spatial) {
+        socket.emit('console dump', "Attempting spatial kill...");
+        spatial.kill('QUIT');
+    } else {
+        socket.emit('console dump', " [ERROR] Cannot kill spatial. Is it running?");
+    }
 }
 
 http.listen(3000, function(){
