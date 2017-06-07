6edb725b7281ecd7ba387f5dcb541003a6091f48
Alex Haak <alex_haak@hotmail.co.uk>
2017-06-07 13:48:07 +0100

    Better console logic

--- proxy/index.js
+++ proxy/index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var child = require('child_process');
 
+let spatialProcess = null;
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -10,18 +12,35 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    let spatial = child.spawn('bash', ['startspatial'], []);
-
-    spatial.stdout.on('data', data => {
-        console.log(data);
-        socket.emit('console dump', data.toString());
-        // res.write(data);
-    });
-
-    spatial.stderr.on('data', data => {
-        // res.send("[PROXY] Something went wrong: " + data);
+    socket.on('request launch', () => {
+        if (startSpatial(socket)) {
+            socket.emit('console dump', "Initiating spatial launch...");
+        } else {
+            socket.emit('console dump', "[ERROR] Spatial already running!");
+        }
     })
+
+
 });
+
+function startSpatial(socket) {
+    if (!spatialProcess) {
+        spatial = child.spawn('bash', ['startspatial'], []);
+
+        spatial.stdout.on('data', data => {
+            console.log(data.toString());
+            socket.emit('console dump', data.toString());
+        });
+
+        spatial.stderr.on('data', err => {
+            socket.emit('console dump', err.toString());
+        })
+
+        return true;
+    }
+
+    return false;
+}
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
